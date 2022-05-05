219e7183d3c7136d2483007229784419f5839efa
Chris Park <102260802+aloeveraornah@users.noreply.github.com>
2022-05-05 12:17:44 -0700

    Update passwordmanager.py

--- passwordmanager.py
+++ passwordmanager.py
@@ -1,5 +1,5 @@
-from cryptography.fernet import Fernet
 
+#the multi line comments below are used to create and open another file that holds the encryption for the master password
 '''
 def write_key():
     key = Fernet.generate_key()
@@ -9,8 +9,8 @@ def write_key():
 write_key()
 '''
 
-def load_key():
-    file = open("key.key", "rb")
+def load_key(): 
+    file = open("key.key", "rb") #this line opens the file "key.key" which holds the input encrypted master password
     key = file.read()
     file.close()
     return key
@@ -18,23 +18,25 @@ def load_key():
 master = input("What is the master password? ")
 key = load_key() + (master.encode())
 fer = Fernet(key)
-
-def view():
-    with open("passwords.txt", "r") as f:
+ 
+def view(): 
+    with open("passwords.txt", "r") as f: #this line opens the file "passowords.txt" and the input encrypted information of the user is printed here
         for line in f.readlines():
             data = line.rstrip()
             user, passcode, number = data.split("|")
             print("User: ", user, "| Password: ", fer.decrypt(passcode.encode()).decode(), "| Phone number: ", 
             fer.decrypt(number.encode()).decode())
+            #the two lines above decrypts the encrypted information (from function add()) of the user and prints them out
  
-def add(): 
+def add(): #this prompts users to enter their name, password, and phone number
     name = input("Account name: ")
     password = input("Password: ")
     phone_number = input("Phone number: ")
     with open("passwords.txt", "a") as f:
         f.write(name + "|" + fer.encrypt(password.encode()).decode() + "|"+ fer.encrypt(phone_number.encode()).decode() + "\n")
+        #the line above encrypts what the user inputs so that it cannot be revealed in the other folders
 
-while True:
+while True: #this line assures that the user must either add/view infor or quit in the game
     mode = input("Would you like to add a new password or view existing ones (add, view)? Or type 'q' to quit: ")
     if mode == "add":
         add()
